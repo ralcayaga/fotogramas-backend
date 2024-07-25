8ec2e1ff24ceb50cac821cb0cf673556f68687d1
ralcayag <ralcayaga74@gmail.com>
2024-07-25 14:49:03 -0400

    Creacion clases

---
+++ src/usuario.ts
@@ -0,0 +1,16 @@
+export class Usuario{
+
+    constructor(
+
+       public nombreUsuario: string,
+       public email: string,
+       public password: string,
+       public fechaRegistro: Date,
+       public fotoPerfil: string,
+       public seguidores:  Usuario,
+       public siguiendo:  Usuario,
+       public fotosSubidas:  string
+          
+    ){}
+
+}
